dafa7595b05b52a4ea2349441ad5f9144c0fb821
Roman Korniichuk <jerrat@bigmir.net>
2017-03-19 16:57:25 +0200

    Send data to template

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,12 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Page;
+use App\Portfolio;
+use App\Service;
+use App\Human;
+
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
+        $pages = Page::all();
+        $portfolios = Portfolio::all();
+        $services = Service::all();
+        $humans = Human::all();
         
+        $menu = [];
+        foreach ($pages as $page) {
+            $item = ['title'=>$page->name, 'alias'=>$page->alias];
+            array_push($menu, $item);
+        }
         
-        return view('site.index');
+         
+        array_push($menu, ['title'=>'Services', 'alias'=>'service']);
+        
+        array_push($menu, ['title'=>'Portfolio', 'alias'=>'Portfolio']);
+        
+        array_push($menu, ['title'=>'Clients', 'alias'=>'clients']);
+        
+        array_push($menu, ['title'=>'Team', 'alias'=>'team']);
+        
+        array_push($menu, ['title'=>'Contact', 'alias'=>'contact']);
+        
+        return view('site.index', [
+            'menu'=>$menu,
+            'pages'=>$pages,
+            'services'=>$services,
+            'portfolios'=>$portfolios,
+            'humans'=>$humans
+        ]);
     }
 }
