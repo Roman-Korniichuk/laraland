a696c97b273b8bd8952b97d8684e3b5c318c233a
Roman Korniichuk <jerrat@bigmir.net>
2017-03-20 15:24:58 +0200

    Added displaying of form validation errors and success message

--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -1,3 +1,19 @@
+@if(session('status'))
+<div class='alert alert-success'>
+    {{ session('status') }}
+</div>
+@endif
+
+@if(count($errors) > 0)
+<div class='alert alert-danger'>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
 @if (isset($pages) && is_object($pages))
 
     @foreach ($pages as $k=>$page)
