a6ebe5169ad49b98ddd7124a60a00b4a3037bad7
Roman Korniichuk <jerrat@bigmir.net>
2017-03-20 18:58:17 +0200

    Created templates for admin panel

---
+++ resources/views/admin/header.blade.php
@@ -0,0 +1,26 @@
+
+<div class="container portfolio_title">
+    <!-- Title -->
+    <div class="section-title">
+        <h2>{{ $title }}</h2>
+    </div>
+    <!--/ Title -->
+</div>
+
+<div class="portfolio">
+    
+    <div id='filters' class="sixteen columns">
+        <ul style="padding:0">
+            <li><a href="{{ route('page') }}">
+                <h5>Pages</h5>
+            </li>
+            <li><a href="{{ route('portfolio') }}">
+                <h5>Portfolio</h5>
+            </li>
+            <li><a href="{{ route('service') }}">
+                <h5>Portfolio</h5>
+            </li>
+        </ul>
+    </div>
+    
+</div>
