f2cd517e61851d3230aa46196cbbff0aa50990c0
Roman Korniichuk <jerrat@bigmir.net>
2017-03-22 21:11:53 +0200

    Added humans view in admin panel

---
+++ resources/views/admin/content_humans.blade.php
@@ -0,0 +1,46 @@
+<div style="margin: 0 50px">
+    
+    @if($humans)
+    
+    <table class="table table-hover table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Text</th>
+                <th>Date</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            
+            @foreach ($humans as $k=>$human)
+            
+            <tr>
+                    <td>{{ $human->id }}</td>
+                    <td>{!! Html::link(route('humansEdit', ['human'=>$human->id]), $human->name, ['alt'=>$human->name]) !!}</td>
+                    <td>{{ $human->alias }}</td>
+                    <td>{{ $human->text }}</td>
+                    <td>{{ $human->created_at }}</td>
+                    <td>
+                        {!! Form::open(['url'=>route('humansEdit', ['human'=>$human->id]), 'class'=>'form-horizontal', 'method'=>'POST']) !!}
+                        
+                            {{ method_field('DELETE') }}
+                            {!! Form::button('Delete!', ['class'=>'btn btn-danger', 'type'=>'submit']) !!}
+                        
+                        {!! Form::close()!!}
+                    </td>
+            </tr>
+                    
+            @endforeach
+            
+        </tbody>
+        
+    </table>
+    
+    {!! Html::link(route('humansAdd'), 'New human') !!}
+    
+    @endif
+    
+</div>
