8d1fa5a8bb3843aea8d59d26d7e8b7e52b0bb8bc
Roman Korniichuk <jerrat@bigmir.net>
2017-03-22 21:47:37 +0200

    Humans editing views

---
+++ resources/views/admin/content_humans_edit.blade.php
@@ -0,0 +1,52 @@
+<div class="wrapper container-fluid">
+    {!! Form::open(['url'=>route('humansEdit', ['human'=>$data['id']]), 'class'=>'form-horizontal', 'method'=>'POST', 'enctype'=>'multipart/form-data']) !!}
+    
+        {!! Form::hidden('id', $data['id']) !!}
+        <div class="form-group">
+            {!! Form::label('name', 'Name', ['class'=>'col-xs-2 control-label']) !!}
+            <div class="col-xs-8">
+                {!! Form::text('name', $data['name'], ['class'=>'form-control', 'placeholder'=>'Name']) !!}
+            </div>
+        </div>
+    
+        <div class="form-group">
+            {!! Form::label('position', 'Position', ['class'=>'col-xs-2 control-label']) !!}
+            <div class="col-xs-8">
+                {!! Form::text('position', $data['position'], ['class'=>'form-control', 'placeholder'=>'Position']) !!}
+            </div>
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('text', 'Text:', ['class'=>'col-xs-2 control-label']) !!}
+            <div class="col-xs-8">
+                {!! Form::textarea('text', $data['text'], ['id'=>'editor','class'=>'form-control']) !!}
+            </div>
+        </div>
+
+        <div class="col-xs-offset-2 col-xs-10">
+            {!! Html::image('assets/img/' . $data['image'], '', ['class'=>'img-responsive', 'width'=>'300px']) !!}
+            {!! Form::hidden('image_old', $data['image']) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('image', 'Image', ['class'=>'col-xs-2 control-label']) !!}
+            <div class="col-xs-8">
+                {!! Form::file('image', ['class'=>'filestyle', 'datatbuttonText'=>'Select an image', 'data-buttonName'=>'btn-primary', 'data-placeholder'=>'No file']) !!}
+            </div>
+        </div>
+        
+        <div class="form-group">
+             <div class="col-xs-offset-2 col-xs-10">
+                {!! Form::button('Save!', ['class'=>'btn btn-primary', 'type'=>'submit']) !!}
+            </div>
+        </div>
+    
+    {!! Form::close() !!}
+    
+    
+  <!--  <script>
+        CKEDITOR.replace('editor');
+    </script> -->
+    
+    
+</div>
+
